ce25eba0d599c331c6a1d1319f25e02467dc8a6e
Nicolas Gramlich <nicolasgramlich@gmail.com>
2010-04-01 11:49:17 +0200

    Added AndEngineExamples Project with Example-Launcher.

---
+++ src/org/anddev/andengine/examples/ExampleLauncher.java
@@ -0,0 +1,73 @@
+package org.anddev.andengine.examples;
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class ExampleLauncher extends ListActivity {
+	// ===========================================================
+	// Constants
+	// ===========================================================
+	
+	final Example[] EXAMPLES = Example.values();
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// Create an ArrayAdapter, that will actually make the Strings above appear in the ListView
+		this.setListAdapter(new ArrayAdapter<Example>(this, android.R.layout.simple_list_item_1, EXAMPLES));
+	}
+
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+
+	@Override
+	protected void onListItemClick(final ListView pListView, final View pView, final int pPosition, final long pId) {
+		super.onListItemClick(pListView, pView, pPosition, pId);
+		startActivity(new Intent(this, EXAMPLES[pPosition].CLASS));
+	}
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+
+	private static enum Example {
+		EXAMPLE_MENU(ExampleMenu.class, "Menu Example");
+
+		public final Class<? extends Activity> CLASS;
+		public final String NAME;
+
+		private Example(final Class<? extends Activity> pExampleClass, final String pExampleName) {
+			this.CLASS = pExampleClass;
+			this.NAME = pExampleName;
+		}
+
+		@Override
+		public String toString() {
+			return this.NAME;
+		}
+	}
+}
---
+++ src/org/anddev/andengine/examples/ExampleMenu.java
@@ -0,0 +1,57 @@
+package org.anddev.andengine.examples;
+
+import org.anddev.andengine.engine.Engine;
+import org.anddev.andengine.entity.Scene;
+import org.anddev.andengine.ui.activity.BaseGameActivity;
+
+/**
+ * @author Nicolas Gramlich
+ * @since 11:33:33 - 01.04.2010
+ */
+public class ExampleMenu extends BaseGameActivity {
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+
+	@Override
+	public void onLoadComplete() {
+	}
+
+	@Override
+	public Engine onLoadEngine() {
+		return null;
+	}
+
+	@Override
+	public void onLoadResources() {
+	}
+
+	@Override
+	public Scene onLoadScene() {
+		return null;
+	}
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+}
