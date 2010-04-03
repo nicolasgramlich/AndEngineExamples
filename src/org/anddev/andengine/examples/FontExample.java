61d2e73903ba6793ecaeca9c61d1a3cc230e998d
nico <devnull@localhost>
2010-04-03 23:26:57 +0200

    Added FontExample.

---
+++ src/org/anddev/andengine/examples/FontExample.java
@@ -0,0 +1,91 @@
+package org.anddev.andengine.examples;
+
+import org.anddev.andengine.engine.Engine;
+import org.anddev.andengine.engine.camera.Camera;
+import org.anddev.andengine.engine.options.EngineOptions;
+import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
+import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.anddev.andengine.entity.FPSCounter;
+import org.anddev.andengine.entity.Scene;
+import org.anddev.andengine.entity.text.Text;
+import org.anddev.andengine.entity.text.Text.HorizontalAlign;
+import org.anddev.andengine.opengl.text.Font;
+import org.anddev.andengine.opengl.texture.Texture;
+import org.anddev.andengine.ui.activity.BaseGameActivity;
+
+import android.graphics.Color;
+import android.graphics.Typeface;
+
+/**
+ * @author Nicolas Gramlich
+ * @since 11:54:51 - 03.04.2010
+ */
+public class FontExample extends BaseGameActivity {
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	private static final int CAMERA_WIDTH = 720;
+	private static final int CAMERA_HEIGHT = 480;
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+
+	private Camera mCamera;
+	private Texture mFontTexture;
+	private Font mFont;
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+
+	@Override
+	public Engine onLoadEngine() {
+		this.mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+		return new Engine(new EngineOptions(true, ScreenOrientation.LANDSCAPE, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), this.mCamera));
+	}
+
+	@Override
+	public void onLoadResources() {
+		this.mFontTexture = new Texture(256, 256);
+
+		this.mFont = new Font(this.mFontTexture, Typeface.create(Typeface.DEFAULT, Typeface.BOLD), 24, true, Color.RED);
+
+		this.getEngine().loadTexture(this.mFontTexture);
+		this.getEngine().loadFont(this.mFont);
+	}
+
+	@Override
+	public Scene onLoadScene() {
+		this.getEngine().registerPreFrameHandler(new FPSCounter());
+		final Scene scene = new Scene(1);
+		
+		scene.setBackgroundColor(0.09804f, 0.6274f, 0.8784f);
+		final Text text = new Text(100, 50, this.mFont, "Hello AndEngine!\nYou can write multilined!", HorizontalAlign.CENTER);
+		scene.getTopLayer().addEntity(text);
+
+		return scene;
+	}
+
+	@Override
+	public void onLoadComplete() {
+
+	}
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+}
