65993c3dc39f70bb754fa6a194f446050b490312
Nicolas Gramlich <nicolasgramlich@gmail.com>
2010-05-05 15:59:42 +0200

    TickerText now even more funny.

--- src/org/anddev/andengine/examples/TickerTextExample.java
+++ src/org/anddev/andengine/examples/TickerTextExample.java
@@ -77,7 +77,7 @@ public class TickerTextExample extends BaseExampleGameActivity {
 		final Scene scene = new Scene(1);
 		scene.setBackgroundColor(0.09804f, 0.6274f, 0.8784f);
 		
-		final Text text = new TickerText(50, 100, this.mFont, "There are also ticker texts!\n\nYou'll see the answer to life in... 5 4 3 2 1...\n\n42\n\nIndeed very funny!", HorizontalAlign.CENTER, 5);
+		final Text text = new TickerText(30, 60, this.mFont, "There are also ticker texts!\n\nYou'll see the answer to life & universe in...\n\n5 4 3 2 1...\n\n42\n\nIndeed very funny!", HorizontalAlign.CENTER, 5);
 		text.addShapeModifier(new SequenceModifier(
 							new ParallelModifier(
 									new AlphaModifier(20, 0.0f, 1.0f), 
