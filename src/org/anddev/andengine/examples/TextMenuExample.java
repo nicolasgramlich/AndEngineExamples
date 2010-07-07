24eb6d2a2f940a21d96ca143ebd985736a333222
Nicolas Gramlich <nicolasgramlich@gmail.com>
2010-07-07 17:16:27 +0200

    Stuff unexpectedly not committed in the previous commit. Changed the CustomFontExample.

---
+++ src/org/anddev/andengine/examples/TextMenuExample.java
@@ -0,0 +1,169 @@
+package org.anddev.andengine.examples;
+
+import org.anddev.andengine.engine.Engine;
+import org.anddev.andengine.engine.camera.Camera;
+import org.anddev.andengine.engine.options.EngineOptions;
+import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
+import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.entity.scene.menu.MenuScene;
+import org.anddev.andengine.entity.scene.menu.MenuScene.IOnMenuItemClickListener;
+import org.anddev.andengine.entity.scene.menu.item.ColorfulTextMenuItem;
+import org.anddev.andengine.entity.scene.menu.item.IMenuItem;
+import org.anddev.andengine.entity.shape.modifier.MoveModifier;
+import org.anddev.andengine.entity.sprite.Sprite;
+import org.anddev.andengine.entity.util.FPSLogger;
+import org.anddev.andengine.opengl.font.Font;
+import org.anddev.andengine.opengl.font.FontFactory;
+import org.anddev.andengine.opengl.texture.Texture;
+import org.anddev.andengine.opengl.texture.TextureOptions;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
+import org.anddev.andengine.opengl.texture.region.TextureRegionFactory;
+
+import android.graphics.Color;
+import android.view.KeyEvent;
+
+/**
+ * @author Nicolas Gramlich
+ * @since 01:30:15 - 02.04.2010
+ */
+public class TextMenuExample extends BaseExample implements IOnMenuItemClickListener {
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	private static final int CAMERA_WIDTH = 720;
+	private static final int CAMERA_HEIGHT = 480;
+
+	protected static final int MENU_RESET = 0;
+	protected static final int MENU_QUIT = MENU_RESET + 1;
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+
+	protected Camera mCamera;
+
+	protected Scene mMainScene;
+
+	private Texture mTexture;
+	private TextureRegion mFaceTextureRegion;
+	
+	private Texture mFontTexture;
+	private Font mFont;
+
+	protected MenuScene mMenuScene;
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+
+	@Override
+	public Engine onLoadEngine() {
+		this.mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+		return new Engine(new EngineOptions(true, ScreenOrientation.LANDSCAPE, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), this.mCamera, false));
+	}
+
+	@Override
+	public void onLoadResources() {
+		/* Load Font/Textures. */
+		this.mFontTexture = new Texture(256, 256, TextureOptions.BILINEAR);
+
+		FontFactory.setAssetBasePath("fonts/");
+		this.mFont = FontFactory.createFromAsset(this.mFontTexture, this, "Plok.ttf", 48, true, Color.WHITE);
+		this.mEngine.getTextureManager().loadTexture(this.mFontTexture);
+		this.mEngine.getFontManager().loadFont(this.mFont);
+		
+		/* Load Sprite-Textures. */
+		this.mTexture = new Texture(64, 64, TextureOptions.BILINEAR);
+		this.mFaceTextureRegion = TextureRegionFactory.createFromAsset(this.mTexture, this, "gfx/boxface_menu.png", 0, 0);
+		this.mEngine.getTextureManager().loadTexture(this.mTexture);
+	}
+
+	@Override
+	public Scene onLoadScene() {
+		this.mEngine.registerPreFrameHandler(new FPSLogger());
+
+		this.mMenuScene = this.createMenuScene();
+
+		/* Just a simple scene with an animated face flying around. */
+		this.mMainScene = new Scene(1);
+		this.mMainScene.setBackgroundColor(0.09804f, 0.6274f, 0.8784f);
+
+		final Sprite face = new Sprite(0, 0, this.mFaceTextureRegion);
+		face.addShapeModifier(new MoveModifier(30, 0, CAMERA_WIDTH - face.getWidth(), 0, CAMERA_HEIGHT - face.getHeight()));
+		this.mMainScene.getTopLayer().addEntity(face);
+
+		return this.mMainScene;
+	}
+
+	@Override
+	public void onLoadComplete() {
+
+	}
+
+	@Override
+	public boolean onKeyDown(final int pKeyCode, final KeyEvent pEvent) {
+		if(pKeyCode == KeyEvent.KEYCODE_MENU && pEvent.getAction() == KeyEvent.ACTION_DOWN) {
+			if(this.mMainScene.hasChildScene()) {
+				/* Remove the menu and reset it. */
+				this.mMenuScene.back();
+			} else {
+				/* Attach the menu. */
+				this.mMainScene.setChildScene(this.mMenuScene, false, true);
+			}
+			return true;
+		} else {
+			return super.onKeyDown(pKeyCode, pEvent);
+		}
+	}
+
+	@Override
+	public boolean onMenuItemClicked(final MenuScene pMenuScene, final IMenuItem pMenuItem) {
+		switch(pMenuItem.getID()) {
+			case MENU_RESET:
+				/* Restart the animation. */
+				this.mMainScene.reset();
+
+				/* Remove the menu and reset it. */
+				this.mMainScene.clearChildScene();
+				this.mMenuScene.reset();
+				return true;
+			case MENU_QUIT:
+				/* End Activity. */
+				this.finish();
+				return true;
+			default:
+				return false;
+		}
+	}
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	protected MenuScene createMenuScene() {
+		final MenuScene menuScene = new MenuScene(this.mCamera);
+
+		menuScene.addMenuItem(new ColorfulTextMenuItem(MENU_RESET, this.mFont, "RESET", 1.0f,0.0f,0.0f, 0.0f,0.0f,0.0f));
+		menuScene.addMenuItem(new ColorfulTextMenuItem(MENU_QUIT, this.mFont, "QUIT", 1.0f,0.0f,0.0f, 0.0f,0.0f,0.0f));
+		menuScene.buildAnimations();
+
+		menuScene.setBackgroundEnabled(false);
+
+		menuScene.setOnMenuItemClickListener(this);
+		return menuScene;
+	}
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+}
