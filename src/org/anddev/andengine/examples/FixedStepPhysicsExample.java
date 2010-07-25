4142468442b224bf0fddce153b98e96beda7cdc2
Nicolas Gramlich <nicolasgramlich@gmail.com>
2010-07-25 20:42:23 +0200

    Added FixedStepPhysicsExample. (This time 4 real !!!)

---
+++ src/org/anddev/andengine/examples/FixedStepPhysicsExample.java
@@ -0,0 +1,182 @@
+package org.anddev.andengine.examples;
+
+import org.anddev.andengine.engine.Engine;
+import org.anddev.andengine.engine.camera.Camera;
+import org.anddev.andengine.engine.options.EngineOptions;
+import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
+import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.anddev.andengine.entity.primitive.Rectangle;
+import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.entity.scene.Scene.IOnSceneTouchListener;
+import org.anddev.andengine.entity.scene.background.ColorBackground;
+import org.anddev.andengine.entity.shape.Shape;
+import org.anddev.andengine.entity.sprite.AnimatedSprite;
+import org.anddev.andengine.entity.util.FPSLogger;
+import org.anddev.andengine.extension.physics.box2d.FixedStepPhysicsWorld;
+import org.anddev.andengine.extension.physics.box2d.PhysicsConnector;
+import org.anddev.andengine.extension.physics.box2d.PhysicsFactory;
+import org.anddev.andengine.extension.physics.box2d.PhysicsWorld;
+import org.anddev.andengine.input.touch.TouchEvent;
+import org.anddev.andengine.opengl.texture.Texture;
+import org.anddev.andengine.opengl.texture.TextureOptions;
+import org.anddev.andengine.opengl.texture.region.TextureRegionFactory;
+import org.anddev.andengine.opengl.texture.region.TiledTextureRegion;
+import org.anddev.andengine.sensor.accelerometer.AccelerometerData;
+import org.anddev.andengine.sensor.accelerometer.IAccelerometerListener;
+import org.anddev.andengine.util.Debug;
+
+import android.hardware.SensorManager;
+import android.view.MotionEvent;
+import android.widget.Toast;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+
+/**
+ * @author Nicolas Gramlich
+ * @since 18:47:08 - 19.03.2010
+ */
+public class FixedStepPhysicsExample extends BaseExample implements IAccelerometerListener, IOnSceneTouchListener {
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	private static final int CAMERA_WIDTH = 720;
+	private static final int CAMERA_HEIGHT = 480;
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+
+	private Texture mTexture;
+
+	private TiledTextureRegion mBoxFaceTextureRegion;
+	private TiledTextureRegion mCircleFaceTextureRegion;
+
+	private PhysicsWorld mPhysicsWorld;
+	
+	private int mFaceCount = 0;
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+
+	@Override
+	public Engine onLoadEngine() {
+		Toast.makeText(this, "Touch the screen to add objects.", Toast.LENGTH_SHORT).show();
+		Toast.makeText(this, "The difference to the normal physics example is that here the simulation steps have a fixed size, which makes the simuation reproduceable.", Toast.LENGTH_LONG).show();
+		final Camera camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+		return new Engine(new EngineOptions(true, ScreenOrientation.LANDSCAPE, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), camera));
+	}
+
+	@Override
+	public void onLoadResources() {
+		this.mTexture = new Texture(64, 64, TextureOptions.BILINEAR);
+		TextureRegionFactory.setAssetBasePath("gfx/");
+		this.mBoxFaceTextureRegion = TextureRegionFactory.createTiledFromAsset(this.mTexture, this, "boxface_tiled.png", 0, 0, 2, 1); // 64x32
+		this.mCircleFaceTextureRegion = TextureRegionFactory.createTiledFromAsset(this.mTexture, this, "circleface_tiled.png", 0, 32, 2, 1); // 64x32
+		this.mEngine.getTextureManager().loadTexture(this.mTexture);
+
+		this.enableAccelerometerSensor(this);
+	}
+
+	@Override
+	public Scene onLoadScene() {
+		this.mEngine.registerPostFrameHandler(new FPSLogger());
+
+		final Scene scene = new Scene(2);
+		scene.setBackground(new ColorBackground(0, 0, 0));
+		scene.setOnSceneTouchListener(this);
+
+		this.mPhysicsWorld = new FixedStepPhysicsWorld(30, new Vector2(0, 2 * SensorManager.GRAVITY_EARTH), false, 3, 2);
+
+		final Shape ground = new Rectangle(0, CAMERA_HEIGHT - 2, CAMERA_WIDTH, 2);
+		final Shape roof = new Rectangle(0, 0, CAMERA_WIDTH, 2);
+		final Shape left = new Rectangle(0, 0, 2, CAMERA_HEIGHT);
+		final Shape right = new Rectangle(CAMERA_WIDTH - 2, 0, 2, CAMERA_HEIGHT);
+
+		final FixtureDef wallFixtureDef = PhysicsFactory.createFixtureDef(0, 0.5f, 0.5f);
+		PhysicsFactory.createBoxBody(this.mPhysicsWorld, ground, BodyType.StaticBody, wallFixtureDef);
+		PhysicsFactory.createBoxBody(this.mPhysicsWorld, roof, BodyType.StaticBody, wallFixtureDef);
+		PhysicsFactory.createBoxBody(this.mPhysicsWorld, left, BodyType.StaticBody, wallFixtureDef);
+		PhysicsFactory.createBoxBody(this.mPhysicsWorld, right, BodyType.StaticBody, wallFixtureDef);
+
+		scene.getBottomLayer().addEntity(ground);
+		scene.getBottomLayer().addEntity(roof);
+		scene.getBottomLayer().addEntity(left);
+		scene.getBottomLayer().addEntity(right);
+
+		scene.registerPreFrameHandler(this.mPhysicsWorld);
+
+		return scene;
+	}
+
+	public void onLoadComplete() {
+
+	}
+
+	@Override
+	public boolean onSceneTouchEvent(final Scene pScene, final TouchEvent pSceneTouchEvent) {
+		if(this.mPhysicsWorld != null) {
+			if(pSceneTouchEvent.getAction() == MotionEvent.ACTION_DOWN) {
+				this.runOnUpdateThread(new Runnable() {
+					@Override
+					public void run() {
+						FixedStepPhysicsExample.this.addFace(pSceneTouchEvent.getX(), pSceneTouchEvent.getY());
+					}
+				});
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public void onAccelerometerChanged(final AccelerometerData pAccelerometerData) {
+		this.mPhysicsWorld.setGravity(new Vector2(10 * pAccelerometerData.getY(), 10 * pAccelerometerData.getX()));
+	}
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	private void addFace(final float pX, final float pY) {
+		final Scene scene = this.mEngine.getScene();
+		
+		this.mFaceCount++;
+		Debug.d("Faces: " + this.mFaceCount);
+
+		final AnimatedSprite face;
+		final Body body;
+
+		final FixtureDef objectFixtureDef = PhysicsFactory.createFixtureDef(1, 0.5f, 0.5f);
+		
+		if(this.mFaceCount % 2 == 0) {
+			face = new AnimatedSprite(pX, pY, this.mBoxFaceTextureRegion);
+			body = PhysicsFactory.createBoxBody(this.mPhysicsWorld, face, BodyType.DynamicBody, objectFixtureDef);
+		} else {
+			face = new AnimatedSprite(pX, pY, this.mCircleFaceTextureRegion);
+			body = PhysicsFactory.createCircleBody(this.mPhysicsWorld, face, BodyType.DynamicBody, objectFixtureDef);
+		}
+
+		face.animate(200);
+		face.setUpdatePhysics(false);
+		
+		scene.getTopLayer().addEntity(face);
+		this.mPhysicsWorld.registerPhysicsConnector(new PhysicsConnector(face, body, true, true, false, false));
+	}
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+}
