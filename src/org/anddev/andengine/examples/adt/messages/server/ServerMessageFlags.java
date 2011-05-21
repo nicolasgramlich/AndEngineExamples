cb646b91d653d048b850b24f7ec252edf1c2aed6
nico <devnull@localhost>
2011-05-21 12:32:59 +0200

    Updated MultiPlayerExtension examples.

---
+++ src/org/anddev/andengine/examples/adt/messages/server/ServerMessageFlags.java
@@ -0,0 +1,21 @@
+package org.anddev.andengine.examples.adt.messages.server;
+
+/**
+ * @author Nicolas Gramlich
+ * @since 11:59:39 - 21.05.2011
+ */
+public interface ServerMessageFlags {
+	// ===========================================================
+	// Final Fields
+	// ===========================================================
+
+	/* Connection Flags. */
+	public static final short FLAG_MESSAGE_SERVER_CONNECTION_CLOSE = Short.MIN_VALUE;
+	public static final short FLAG_MESSAGE_SERVER_CONNECTION_ESTABLISHED = FLAG_MESSAGE_SERVER_CONNECTION_CLOSE + 1;
+	public static final short FLAG_MESSAGE_SERVER_CONNECTION_REJECTED_PROTOCOL_MISSMATCH = FLAG_MESSAGE_SERVER_CONNECTION_ESTABLISHED + 1;
+	public static final short FLAG_MESSAGE_SERVER_CONNECTION_PONG = FLAG_MESSAGE_SERVER_CONNECTION_REJECTED_PROTOCOL_MISSMATCH + 1;
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+}
