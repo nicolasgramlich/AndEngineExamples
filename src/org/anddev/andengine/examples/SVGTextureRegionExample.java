7455f7092a718dc67b24a1610d6132b815d49e47
NicolasGramlich <devnull@localhost>
2011-05-25 20:42:35 +0200

    Updated SVGTextureRegionExample.

--- src/org/anddev/andengine/examples/SVGTextureRegionExample.java
+++ src/org/anddev/andengine/examples/SVGTextureRegionExample.java
@@ -7,6 +7,7 @@ import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
 import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.anddev.andengine.entity.scene.Scene;
 import org.anddev.andengine.entity.scene.background.ColorBackground;
+import org.anddev.andengine.entity.sprite.AnimatedSprite;
 import org.anddev.andengine.entity.sprite.Sprite;
 import org.anddev.andengine.entity.util.FPSLogger;
 import org.anddev.andengine.extension.svg.opengl.texture.region.SVGTextureRegionFactory;
@@ -14,12 +15,15 @@ import org.anddev.andengine.opengl.texture.BuildableTexture;
 import org.anddev.andengine.opengl.texture.TextureOptions;
 import org.anddev.andengine.opengl.texture.builder.BlackPawnTextureBuilder;
 import org.anddev.andengine.opengl.texture.builder.ITextureBuilder.TextureSourcePackingException;
+import org.anddev.andengine.opengl.texture.region.BaseTextureRegion;
 import org.anddev.andengine.opengl.texture.region.TextureRegion;
+import org.anddev.andengine.opengl.texture.region.TiledTextureRegion;
 import org.anddev.andengine.util.Debug;
 
 import android.graphics.Color;
 
 import com.larvalabs.svgandroid.adt.ISVGColorMapper;
+import com.larvalabs.svgandroid.adt.SVGDirectColorMapper;
 
 /**
  * @author Nicolas Gramlich
@@ -35,8 +39,8 @@ public class SVGTextureRegionExample extends BaseExample {
 
 	private static final int SIZE = 128;
 	
-	private static final int COUNT = 6;
-	private static final int COLUMNS = 3;
+	private static final int COUNT = 12;
+	private static final int COLUMNS = 4;
 	private static final int ROWS = (int)Math.ceil((float)COUNT / COLUMNS);
 
 	// ===========================================================
@@ -45,7 +49,7 @@ public class SVGTextureRegionExample extends BaseExample {
 
 	private Camera mCamera;
 	private BuildableTexture mBuildableTexture;
-	private TextureRegion[] mSVGTestTextureRegions;
+	private BaseTextureRegion[] mSVGTestTextureRegions;
 
 	// ===========================================================
 	// Constructors
@@ -70,13 +74,25 @@ public class SVGTextureRegionExample extends BaseExample {
 		this.mBuildableTexture = new BuildableTexture(1024, 1024, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
 		SVGTextureRegionFactory.setAssetBasePath("gfx/");
 
-		this.mSVGTestTextureRegions = new TextureRegion[COUNT];
+		this.mSVGTestTextureRegions = new BaseTextureRegion[COUNT];
 		int i = 0;
 		this.mSVGTestTextureRegions[i++] = SVGTextureRegionFactory.createFromAsset(this.mBuildableTexture, this, "chick.svg", 16, 16);
+		this.mSVGTestTextureRegions[i++] = SVGTextureRegionFactory.createFromAsset(this.mBuildableTexture, this, "chick.svg", 32, 32);
 		this.mSVGTestTextureRegions[i++] = SVGTextureRegionFactory.createFromAsset(this.mBuildableTexture, this, "chick.svg", 64, 64);
-		this.mSVGTestTextureRegions[i++] = SVGTextureRegionFactory.createFromAsset(this.mBuildableTexture, this, "chick.svg", 256, 256);
+		this.mSVGTestTextureRegions[i++] = SVGTextureRegionFactory.createFromAsset(this.mBuildableTexture, this, "chick.svg", 128, 128);
 		this.mSVGTestTextureRegions[i++] = SVGTextureRegionFactory.createFromAsset(this.mBuildableTexture, this, "badge.svg", 16, 16);
 		this.mSVGTestTextureRegions[i++] = SVGTextureRegionFactory.createFromAsset(this.mBuildableTexture, this, "badge.svg", 64, 64);
+		this.mSVGTestTextureRegions[i++] = SVGTextureRegionFactory.createFromAsset(this.mBuildableTexture, this, "badge.svg", 128, 128, new ISVGColorMapper() {
+			@Override
+			public Integer mapColor(Integer pColor) {
+				if(pColor == null) {
+					return null;
+				} else {
+					/* Swap blue and green channel. */
+					return Color.argb(0, Color.red(pColor), Color.blue(pColor), Color.green(pColor));
+				}
+			}
+		});
 		this.mSVGTestTextureRegions[i++] = SVGTextureRegionFactory.createFromAsset(this.mBuildableTexture, this, "badge.svg", 256, 256, new ISVGColorMapper() {
 			@Override
 			public Integer mapColor(Integer pColor) {
@@ -88,6 +104,13 @@ public class SVGTextureRegionExample extends BaseExample {
 				}
 			}
 		});
+		this.mSVGTestTextureRegions[i++] = SVGTextureRegionFactory.createTiledFromAsset(this.mBuildableTexture, this, "pacdroid.svg", 64, 64, 2, 2);
+		this.mSVGTestTextureRegions[i++] = SVGTextureRegionFactory.createTiledFromAsset(this.mBuildableTexture, this, "pacdroid.svg", 256, 256, 2, 2);
+		final SVGDirectColorMapper angryPacDroidSVGColorMapper = new SVGDirectColorMapper();
+		angryPacDroidSVGColorMapper.addColorMapping(0xA7CA4A, 0xEA872A);
+		angryPacDroidSVGColorMapper.addColorMapping(0xC1DA7F, 0xFAA15F);
+		this.mSVGTestTextureRegions[i++] = SVGTextureRegionFactory.createTiledFromAsset(this.mBuildableTexture, this, "pacdroid.svg", 256, 256, angryPacDroidSVGColorMapper, 2, 2);
+		this.mSVGTestTextureRegions[i++] = SVGTextureRegionFactory.createTiledFromAsset(this.mBuildableTexture, this, "pacdroid_apples.svg", 256, 256, 2, 2);
 
 		try {
 			this.mBuildableTexture.build(new BlackPawnTextureBuilder(1));
@@ -103,7 +126,7 @@ public class SVGTextureRegionExample extends BaseExample {
 		this.mEngine.registerUpdateHandler(new FPSLogger());
 
 		final Scene scene = new Scene(0);
-		scene.setBackground(new ColorBackground(1, 1, 1));
+		scene.setBackground(new ColorBackground(0.5f, 0.5f, 0.5f));
 		
 		for(int i = 0; i < COUNT; i++) {
 			final int row = i / COLUMNS;
@@ -114,7 +137,16 @@ public class SVGTextureRegionExample extends BaseExample {
 			
 			final float x = centerX - SIZE * 0.5f;
 			final float y = centerY - SIZE * 0.5f;
-			scene.attachChild(new Sprite(x, y, SIZE, SIZE, this.mSVGTestTextureRegions[i]));
+			final BaseTextureRegion baseTextureRegion = this.mSVGTestTextureRegions[i];
+			if(baseTextureRegion instanceof TextureRegion) {
+				final TextureRegion textureRegion = (TextureRegion)baseTextureRegion;
+				scene.attachChild(new Sprite(x, y, SIZE, SIZE, textureRegion));
+			} else if(baseTextureRegion instanceof TiledTextureRegion) {
+				final TiledTextureRegion tiledTextureRegion = (TiledTextureRegion)baseTextureRegion;
+				final AnimatedSprite animatedSprite = new AnimatedSprite(x, y, SIZE, SIZE, tiledTextureRegion);
+				animatedSprite.animate(500);
+				scene.attachChild(animatedSprite);
+			}
 		}
 
 		return scene;
