3e71933bbb57eaaf892ae8c56e885a21d108b71a
Nicolas Gramlich <ngramlich@zynga.com>
2012-03-05 20:13:29 -0800

    Updated ScriptingExample.

--- src/org/andengine/examples/ScriptingExample.java
+++ src/org/andengine/examples/ScriptingExample.java
@@ -1,20 +1,28 @@
 package org.andengine.examples;
 
+import java.io.IOException;
+
+import org.andengine.engine.Engine;
 import org.andengine.engine.camera.Camera;
 import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.EngineOptions.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
-import org.andengine.entity.IEntity;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
+import org.andengine.entity.sprite.Sprite;
 import org.andengine.entity.util.FPSLogger;
 import org.andengine.extension.scripting.ScriptingEnvironment;
 import org.andengine.extension.scripting.Test;
-import org.andengine.ui.activity.SimpleLayoutGameActivity;
+import org.andengine.opengl.texture.ITexture;
+import org.andengine.opengl.texture.bitmap.AssetBitmapTexture;
+import org.andengine.opengl.texture.region.ITextureRegion;
+import org.andengine.opengl.texture.region.TextureRegionFactory;
+import org.andengine.ui.activity.LayoutGameActivity;
 import org.andengine.util.debug.Debug;
 import org.andengine.util.system.SystemUtils;
 
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
@@ -26,10 +34,16 @@ import android.widget.EditText;
  * @author Nicolas Gramlich
  * @since 11:54:51 - 03.04.2010
  */
-public class ScriptingExample extends SimpleLayoutGameActivity implements OnClickListener {
+public class ScriptingExample extends LayoutGameActivity implements OnClickListener {
 	// ===========================================================
 	// Constants
 	// ===========================================================
+
+	static {
+		System.loadLibrary("gnustl_shared");
+//		System.loadLibrary("js");
+		System.loadLibrary("andenginescriptingextension");
+	}
 
 	private static final int CAMERA_WIDTH = 720;
 	private static final int CAMERA_HEIGHT = 480;
@@ -44,6 +58,18 @@ public class ScriptingExample extends SimpleLayoutGameActivity implements OnClic
 	// Constructors
 	// ===========================================================
 
+	@Override
+	protected void onCreate(Bundle pSavedInstanceState) {
+		try {
+			final String apkFilePath = SystemUtils.getApkFilePath(this);
+			ScriptingEnvironment.init(this, apkFilePath);
+		} catch (final NameNotFoundException e) {
+			Debug.e(e);
+		}
+
+		super.onCreate(pSavedInstanceState);
+	}
+
 	// ===========================================================
 	// Getter & Setter
 	// ===========================================================
@@ -51,9 +77,6 @@ public class ScriptingExample extends SimpleLayoutGameActivity implements OnClic
 	// ===========================================================
 	// Methods for/from SuperClass/Interfaces
 	// ===========================================================
-
-//	public static native String stringFromJNI();
-//	public static native String unimplementedStringFromJNI();
 
 	@Override
 	protected int getLayoutID() {
@@ -73,34 +96,46 @@ public class ScriptingExample extends SimpleLayoutGameActivity implements OnClic
 	}
 
 	@Override
+	public Engine onCreateEngine(final EngineOptions pEngineOptions) {
+		return ScriptingEnvironment.onCreateEngine(pEngineOptions);
+	}
+
+	@Override
 	protected void onSetContentView() {
 		super.onSetContentView();
 
 		this.mEditTextCode = (EditText) this.findViewById(R.id.scriptingexample_code);
-		this.mEditTextCode.setText("p00p");
+		try {
+			this.mEditTextCode.setText(SystemUtils.getApkFilePath(this));
+		} catch (final NameNotFoundException e) {
+			Debug.e(e);
+		}
 		this.findViewById(R.id.scriptingexample_code_apply).setOnClickListener(this);
 	}
 
 	@Override
-	public void onCreateResources() {
-		try {
-			ScriptingEnvironment.init(SystemUtils.getApkFilePath(this), this.getEngine());
-		} catch (NameNotFoundException e) {
-			Debug.e(e);
-		}
+	public void onCreateResources(final OnCreateResourcesCallback pOnCreateResourcesCallback) throws Exception {
+		pOnCreateResourcesCallback.onCreateResourcesFinished();
 	}
 
 	@Override
-	public Scene onCreateScene() {
+	public void onCreateScene(final OnCreateSceneCallback pOnCreateSceneCallback) throws Exception {
 		this.mEngine.registerUpdateHandler(new FPSLogger());
 
 		final Scene scene = new Scene();
 		scene.setOnAreaTouchTraversalFrontToBack();
-		scene.setBackground(new Background(1, 0, 0, 1));
+		scene.setBackground(new Background(1, 1, 1, 1));
+		
+		final Sprite rectangle = (Sprite)Test.test();
+		scene.attachChild(rectangle);
+		scene.registerTouchArea(rectangle);
 
-		scene.attachChild((IEntity)Test.test());
+		pOnCreateSceneCallback.onCreateSceneFinished(scene);
+	}
 
-		return scene;
+	@Override
+	public void onPopulateScene(final Scene pScene, final OnPopulateSceneCallback pOnPopulateSceneCallback) throws Exception {
+		pOnPopulateSceneCallback.onPopulateSceneFinished();
 	}
 
 	@Override
